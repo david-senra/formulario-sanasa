a5e19a98b32174cfb1830c2c64a2f59d5d9bd0fc
David Senra <davidsenra33@gmail.com>
2023-06-15 23:17:29 -0300

    inicial aqui

---
+++ main.js
@@ -0,0 +1,246 @@
+var blob = [];
+var nome = "";
+
+var estaCasado = true;
+var estaEmComunhao = true;
+var temEscrituraPublica = true;
+var temPartilha = true;
+var temContratoParticular = true;
+var temContratoParticularEmLei = true;
+
+const mensagemSucesso = document.getElementsByClassName('success-message');
+const elementoHeader = document.getElementsByClassName('header');
+
+const stateEstadoCivil = document.getElementsByClassName('select-estado-civil');
+const stateRegimeBens = document.getElementsByClassName('select-regime-bens');
+
+const listaRegimeBens = document.getElementById('regime-bens');
+const listaConjuge = document.getElementById('ul-conjuge');
+const selectComunhao = document.getElementById('select-comunhao');
+
+const stateContratoImovel = document.getElementsByClassName('select-contrato-imovel');
+const stateContratoParticular = document.getElementsByClassName('select-contrato-particular');
+
+const listaEscrituraPublica = document.getElementsByClassName('div-contrato-escritura');
+const listaContratoPartilha = document.getElementsByClassName('div-contrato-partilha');
+const listaContratoParticular = document.getElementsByClassName('div-contrato-particular');
+const listaLei = document.getElementsByClassName('lista-lei');
+
+const botaoGerador = document.getElementsByClassName('botao-gerar');
+const botaoBaixador = document.getElementsByClassName('botao-download');
+
+const formulario = document.getElementsByClassName('form');
+
+const todosInputs = document.getElementsByClassName('inputs');
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    function setConfiguracaoCivil() {
+        if (stateEstadoCivil[0].value === 'casado(a)' && stateRegimeBens[0].value !== 'comunhão') {
+            listaRegimeBens.classList.remove('hide');
+            listaConjuge.classList.remove('hide');
+            selectComunhao.classList.add('hide');
+            estaCasado = true;
+            estaEmComunhao = false;
+        }
+        else if (stateEstadoCivil[0].value === 'casado(a)' && stateRegimeBens[0].value === 'comunhão') {
+            selectComunhao.classList.remove('hide');
+            listaRegimeBens.classList.remove('hide');
+            listaConjuge.classList.remove('hide');
+            estaCasado = true;
+            estaEmComunhao = true;
+        }
+        else {
+            selectComunhao.classList.add('hide');
+            listaRegimeBens.classList.add('hide');
+            listaConjuge.classList.add('hide');
+            estaCasado = false;
+            estaEmComunhao = false;
+        }
+    }
+
+    stateEstadoCivil[0].addEventListener('change', setConfiguracaoCivil);
+    stateRegimeBens[0].addEventListener('change', setConfiguracaoCivil);
+
+    function setConfiguracaoContratos() {
+        if (stateContratoImovel[0].value === 'Escritura Pública') {
+            listaEscrituraPublica[0].classList.remove('hide');
+            listaContratoParticular[0].classList.add('hide');
+            listaContratoPartilha[0].classList.add('hide');
+            temEscrituraPublica = true;
+            temPartilha = false;
+            temContratoParticular = false;
+            temContratoParticularEmLei = false;
+        }
+        else if (stateContratoImovel[0].value === 'Forma de Partilha') {
+            listaEscrituraPublica[0].classList.add('hide');
+            listaContratoParticular[0].classList.add('hide');
+            listaContratoPartilha[0].classList.remove('hide');
+            temEscrituraPublica = false;
+            temPartilha = true;
+            temContratoParticular = false;
+            temContratoParticularEmLei = false;
+        }
+        else if (stateContratoParticular[0].value === 'Regulado por Lei') {
+            listaEscrituraPublica[0].classList.add('hide');
+            listaContratoParticular[0].classList.remove('hide');
+            listaContratoPartilha[0].classList.add('hide');
+            listaLei[0].classList.remove('hide');
+            temEscrituraPublica = false;
+            temPartilha = false;
+            temContratoParticular = true;
+            temContratoParticularEmLei = true;
+        }
+        else {
+            listaEscrituraPublica[0].classList.add('hide');
+            listaContratoParticular[0].classList.remove('hide');
+            listaContratoPartilha[0].classList.add('hide');
+            listaLei[0].classList.add('hide');
+            temEscrituraPublica = false;
+            temPartilha = false;
+            temContratoParticular = true;
+            temContratoParticularEmLei = false;
+        }
+    }
+
+    stateContratoImovel[0].addEventListener('change', setConfiguracaoContratos);
+    stateContratoParticular[0].addEventListener('change', setConfiguracaoContratos);
+
+    botaoGerador[0].addEventListener('click', generate);
+    botaoBaixador[0].addEventListener('click', downloadFile);
+
+    
+})
+
+function loadFile(url, callback) {
+    PizZipUtils.getBinaryContent(url, callback);
+}
+
+function generate() {
+    const todosErros = document.getElementsByTagName('P');
+    const arrayErros = Array.from(todosErros);
+    arrayErros.forEach(element => {
+        element.remove();
+    })
+    const outrosErros = document.getElementsByClassName('loucura');
+    const outraarrayErros = Array.from(outrosErros);
+    outraarrayErros.forEach(element => {
+        element.remove();
+    })
+    const inputsarray = Array.from(todosInputs);
+    let contador = 0;
+    inputsarray.forEach(input => {
+        if (input.offsetParent !== null && input.value == '') {
+            var newElement = document.createElement('div')
+            const elem = document.createElement("p");
+            const text = document.createTextNode("Não deixe campos em branco!");
+            elem.appendChild(text);
+            newElement.appendChild(elem);
+            elem.classList.add('error');
+            newElement.classList.add('loucura');
+            (input.parentNode).parentElement.insertBefore(newElement, input.parentNode.nextSibling);
+            if (input.tagName == 'SELECT') {
+                elem.classList.add('error--extra');
+            }
+            const mensagemErroDois = document.getElementById('erro-maior')
+            mensagemErroDois.classList.add('error-two--active');
+            contador++;
+            input.addEventListener('keydown', e => {
+                elem.remove();
+                mensagemErroDois.classList.remove('error-two--active')
+            })
+        } 
+    })
+    if (contador > 0) {
+        return;
+    }
+    loadFile(
+        "template.docx",
+        function (error, content) {
+            if (error) {
+                throw error;
+            }
+            const zip = new PizZip(content);
+            const doc = new window.docxtemplater(zip, {
+                paragraphLoop: true,
+                linebreaks: true,
+            });
+            console.log(stateEstadoCivil[0].value)
+            // Render the document (Replace {first_name} by John, {last_name} by Doe, ...) 
+            doc.render({
+                'casado': estaCasado,
+                'comunhao': estaEmComunhao,
+                'escritura': temEscrituraPublica,
+                'partilha' : temPartilha,
+                'particular' : temContratoParticular,
+                'regulado': temContratoParticularEmLei,
+                '1': document.getElementById('1').value,
+                '2': document.getElementById('2').value,
+                '3': document.getElementById('3').value,
+                '4': document.getElementById('4').value,
+                '5': document.getElementById('5').value,
+                '6': document.getElementById('6').value,
+                '7': document.getElementById('7').value,
+                '8': document.getElementById('8').value,
+                '9': document.getElementById('9').value,
+                '10': document.getElementById('10').value,
+                '11': document.getElementById('11').value,
+                '12': document.getElementById('12').value,
+                '13': document.getElementById('13').value,
+                '14': document.getElementById('14').value,
+                '15': document.getElementById('15').value,
+                '16': document.getElementById('16').value,
+                '17': document.getElementById('17').value,
+                '18': document.getElementById('18').value,
+                '19': document.getElementById('19').value,
+                '20': document.getElementById('20').value,
+                '21': document.getElementById('21').value,
+                '22': document.getElementById('22').value,
+                '23': document.getElementById('23').value,
+                '24': document.getElementById('24').value,
+                '25': document.getElementById('25').value,
+                '26': document.getElementById('26').value,
+                '27': document.getElementById('27').value,
+                '28': document.getElementById('28').value,
+                '29': document.getElementById('29').value,
+                '30': document.getElementById('30').value,
+                '31': document.getElementById('31').value,
+                '32': document.getElementById('32').value,
+                '33': document.getElementById('33').value,
+                '34': document.getElementById('34').value,
+                '35': document.getElementById('35').value,
+                '36': document.getElementById('36').value,
+                '37': document.getElementById('37').value,
+                '38': document.getElementById('38').value,
+                '39': document.getElementById('39').value,
+                '40': document.getElementById('40').value,
+                '41': document.getElementById('41').value,
+                '42': document.getElementById('42').value,
+                '43': document.getElementById('43').value,
+            });
+            
+            blob = doc.getZip().generate({
+                type: "blob",
+                mimeType:
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                // compression: DEFLATE adds a compression step.
+                // For a 50MB output document, expect 500ms additional CPU time
+                compression: "DEFLATE",
+            });
+            // Output the document using Data-URI
+            
+            const nomeArquivo = document.getElementById('1').value;
+            nome = nomeArquivo.replace(/\s+/g, '-').toLowerCase();
+            formulario[0].classList.add('hide');
+            botaoBaixador[0].classList.remove('hide');
+            mensagemSucesso[0].classList.remove('hide');
+            elementoHeader[0].classList.add('party');
+        }
+    );
+};
+
+function downloadFile() {
+    saveAs(blob, `${nome}-sanasa.docx`);
+}
